63564b180067f1d83911b2b67bf3a1c8aa4c25b0
@claudenes <claudenes@gmail.com>
2022-02-16 12:57:58 -0300

    Implementações CQRS

--- CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -10,9 +10,9 @@ namespace CleanArchMvc.Application.Interfaces
     public interface IProductService
     {
         Task<IEnumerable<ProductDTO>> GetProducts();
-        Task<ProductDTO> GetById(int? id);
-        Task Add(ProductDTO productDto);
-        Task Update(ProductDTO productDto);
-        Task Remove(int? id);
+        //Task<ProductDTO> GetById(int? id);
+        //Task Add(ProductDTO productDto);
+        //Task Update(ProductDTO productDto);
+        //Task Remove(int? id);
     }
 }
